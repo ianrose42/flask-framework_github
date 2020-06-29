437f21e15966b69bf3321055abac4dc391a737a4
Ian Rose <imr923@gmail.com>
2020-06-29 04:39:36 -0400

    stage_12

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
  # return render_template('index.html')
- return render_template('NYS.html')
+ return render_template('NYS2.html')
 
 @app.route('/about')
 def about():
